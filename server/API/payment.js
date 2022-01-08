4614ab07aa60dd9ff6074f12fb78ae7594ac9427
omarnahtawy <32402719+Twitchhx@users.noreply.github.com>
2022-01-08 18:23:14 +0100

    Update payment.js

--- server/API/payment.js
+++ server/API/payment.js
@@ -1,13 +1,21 @@
 require('dotenv').on("load");
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+//const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-var stripeHandler = StripeCheckout.configure({
-    key:stripePublicKey,
-    locale: auto,
+app.post('/payment', async (req,res)=>{
+    try{
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: req.body,
+        currency: 'egp',
+        payment_method_types: ['card'],
+      });
+      res.status(200).send("Payment Successful:", paymentIntent);
+    }catch(err){
+        res.status(400).send("Error:", err);
+    }
+
+
+    //stripeHandler.redirectToCheckout();
 });
-export const payment = () =>{
 
-    stripeHandler.redirectToCheckout();
-}
 export default payment;
